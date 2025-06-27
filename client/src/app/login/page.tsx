478cc8353f8a7da30b2607259f034893b9f528f2
Daniel <danielrmendonca04@gmail.com>
2025-06-27 12:08:24 -0300

    tela de login
    formulario de login em components e tela de login em app

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/loginform/loginform";
+
+export default async function Login() {
+  return (
+    <section className="formLogin">
+      <LoginForm/>
+    </section>
+  );
+}

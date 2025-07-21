2b46489c1b8a02a0517f2ba33564f58a8e89a0c0
Lucas Torres <lucastorreslr10@gmail.com>
2025-07-21 14:30:42 -0300

    refactor: removed min-h-screen

--- client/src/app/ong-home/layout.tsx
+++ client/src/app/ong-home/layout.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import { ReactNode } from "react";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 export default function OngHomeLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen flex flex-col">
-      
+    <div className="flex flex-col">
       <main className="flex-1">{children}</main>
-      
     </div>
   );
 }

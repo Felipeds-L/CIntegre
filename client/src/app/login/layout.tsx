fbfc4bfcb67b22aa465cb676edf6f561e4014b09
Lucas Torres <lucastorreslr10@gmail.com>
2025-07-21 15:24:46 -0300

    fix: login layout responsiveness

--- client/src/app/login/layout.tsx
+++ client/src/app/login/layout.tsx
@@ -6,8 +6,8 @@ export default async function LoginLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="grid grid-cols-2 gap-[2rem] max-h-screen">
-      <div>
+    <div className="grid grid-cols-2 gap-8 h-full max-[68.75rem]:grid-cols-1">
+      <div className="max-[68.75rem]:hidden relative h-full">
         <Image
           src={
             "https://plus.unsplash.com/premium_photo-1679429320652-4a9bb7179c4e?q=100&w=640&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -15,10 +15,10 @@ export default async function LoginLayout({
           width={300}
           height={900}
           alt=""
-          className="w-full h-full object-cover max-h-screen"
+          className="w-full max-h-screen object-cover"
         />
       </div>
-      <div className="max-w-[30rem] p-[1rem] max-sm:max-w-full mt-[20vh]">
+      <div className="max-w-[30rem] p-4 max-[68.75rem]:px-12 max-[68.75rem]:max-w-full mt-[20vh]">
         {children}
       </div>
     </div>

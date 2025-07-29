c2ca96b62309c588ec31b4e284efa2a1875cce94
Felipe Leite <felipesd.leite@gmail.com>
2025-07-28 21:30:43 -0300

    fix: solving schoolDTO

--- server/src/school/schoolDto.ts
+++ server/src/school/schoolDto.ts
@@ -1,9 +1,12 @@
 import { SchoolAction } from '../schoolAction/schoolActionDto';
 
 export interface School {
-    id: number;
-    cnpj: string;
-    student_number: number;
-    score: number;
-    //schoolAction?: SchoolAction;
+  id: number;
+  name: string;
+  student_quantity: number;
+  score: number;
+  phone_number: string;
+
+  address_id: number;
+  schoolAction?: SchoolAction[];
 }

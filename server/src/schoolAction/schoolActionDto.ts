2e1fc3bb1f0f1242d2e57386dbb4597bf71b169a
Lucas Torres <lucastorreslr10@gmail.com>
2025-07-31 17:18:36 -0300

    fix: remaining dtos

--- server/src/schoolAction/schoolActionDto.ts
+++ server/src/schoolAction/schoolActionDto.ts
@@ -1,27 +1,24 @@
-import { School } from "../school/schoolDto";
-import { Causes } from "../causes/causesDto";
-import { Status } from "@prisma/client";
+import { School } from '../school/schoolDto';
+import { Causes } from '../causes/causesDto';
+import { Status } from '@prisma/client';
+import SocialActionService from '../socialAction/socialActionService';
 
 type SchoolActionStatus = Status;
 
 export interface SchoolAction {
   id: number;
+  status: SchoolActionStatus;
+  pontuation: number; // ong avalia
+  activityId: number;
+  activity: SocialActionService;
   school_id: number;
   school: School;
-  causes_id: number;
-  causes: Causes;
-  status: SchoolActionStatus[]; 
-  registers: string;
-  pontuation: number; // ong avalia 
 }
 
 export interface SchoolActionCreateDTO {
-  //id: number;
   school_id: number;
-  //school: School;
   causes_id: number;
-  //causes: Causes;
-  status: SchoolActionStatus[]; 
+  status: SchoolActionStatus;
   registers: string;
-  pontuation: number; // ong avalia 
+  pontuation: number; // ong avalia
 }

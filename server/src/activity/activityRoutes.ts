bfc8a9debec1e0e3a08af3ce5d4ee1846e7c8e2f
Felipe Leite <felipesd.leite@gmail.com>
2025-08-01 21:12:52 -0300

    refactor: activityRoutes

--- server/src/activity/activityRoutes.ts
+++ server/src/activity/activityRoutes.ts
@@ -1,32 +1,47 @@
 import { Router } from 'express';
 import { ActivityController } from './activityController';
+import { authenticate } from '../middlewares/middleware';
 
-const router = Router();
 const activityController = new ActivityController();
 
 export function setActivityRoutes(app: Router) {
   app.post(
     '/activities',
-    activityController.createActivity.bind(activityController),
+    authenticate,
+    activityController.createActivity.bind(
+      activityController,
+    ),
   );
   app.get(
     '/activities/:id',
+    authenticate,
     activityController.getActivity.bind(activityController),
   );
   app.put(
     '/activities/:id',
-    activityController.updateActivity.bind(activityController),
+    authenticate,
+    activityController.updateActivity.bind(
+      activityController,
+    ),
   );
   app.delete(
     '/activities/:id',
-    activityController.deleteActivity.bind(activityController),
+    authenticate,
+    activityController.deleteActivity.bind(
+      activityController,
+    ),
   );
   app.get(
     '/activities',
-    activityController.getAllActivities.bind(activityController),
+    activityController.getAllActivities.bind(
+      activityController,
+    ),
   );
   app.get(
     '/activities/ong/:ongId',
-    activityController.getActivityByOngId.bind(activityController),
+    authenticate,
+    activityController.getActivityByOngId.bind(
+      activityController,
+    ),
   );
 }

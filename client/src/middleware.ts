aa4e27dcfa0b7d82553bcc257adad1375be96992
Lucas Torres <lucastorreslr10@gmail.com>
2025-08-07 17:21:00 -0300

    feat: middleware to protect routes that need token

---
+++ client/src/middleware.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from "next/server";
+import verifyToken from "./lib/verifyToken";
+
+export async function middleware(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+  const authenticated = token ? await verifyToken(token) : false;
+
+  if (!authenticated && req.nextUrl.pathname.startsWith("/profile")) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (!authenticated && req.nextUrl.pathname.startsWith("/my-activities")) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (!authenticated && req.nextUrl.pathname.startsWith("/ong-home")) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (!authenticated && req.nextUrl.pathname.startsWith("/school-home")) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (authenticated && req.nextUrl.pathname.startsWith("/login")) {
+    return NextResponse.redirect(new URL("/profile", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/profile/:path*",
+    "/login/:path*",
+    "/my-activities/:path*",
+    "/ong-home/:path*",
+    "/school-home/:path*",
+  ],
+};

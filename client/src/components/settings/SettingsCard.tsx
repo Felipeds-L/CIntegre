f89dc267d8ece92fa59184a3b6bb69fb1bad33c1
IsabelleTenorio <isabelle.tenorio05@gmail.com>
2025-08-07 19:04:37 -0300

    fix(fix/hover-logout): fixed the hover of the logout button and created a phrase carousel to the lorem ipsums at the main page

--- client/src/components/settings/SettingsCard.tsx
+++ client/src/components/settings/SettingsCard.tsx
@@ -15,7 +15,7 @@ const SettingsCard: React.FC<SettingsCardProps> = ({
   return (
     <button
       onClick={onClick}
-      className="flex w-full items-center gap-5 rounded-lg border-l-4 border-blue-600 bg-white p-6 shadow-lg transition-all hover:bg-gray-50 hover:shadow-md"
+      className="hover:cursor-pointer flex w-full items-center gap-5 rounded-lg border-l-4 border-blue-600 bg-white p-6 shadow-lg transition-all hover:bg-gray-50 hover:shadow-md"
     >
       <IconComponent className="h-8 w-8 text-blue-600" />
       <span className="text-lg font-medium text-gray-800">{text}</span>

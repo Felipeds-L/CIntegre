ca147e82baa4113944aacceeefbcdfb8e5858ad7
Lucas Torres <lucastorreslr10@gmail.com>
2025-08-14 05:35:39 -0300

    feat: default photos to each category

--- client/src/actions/postActivity.ts
+++ client/src/actions/postActivity.ts
@@ -45,7 +45,7 @@ export default async function postActivity(
       };
     }
 
-    const {data: user} = await getAuthUser();
+    const { data: user } = await getAuthUser();
     if (!user?.ong) {
       return {
         ok: false,
@@ -53,7 +53,6 @@ export default async function postActivity(
         data: null,
       };
     }
-
 
     let tags: string[] = [];
     if (tagsString) {
@@ -92,8 +91,34 @@ export default async function postActivity(
     };
 
     type FieldOfActionKey = keyof typeof fieldOfActionFilter;
+
+    const defaultImageForField = {
+      Educação:
+        "https://images.unsplash.com/photo-1497633762265-9d179a990aa6?q=80&w=1173&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      Saúde:
+        "https://images.unsplash.com/photo-1526256262350-7da7584cf5eb?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "Meio Ambiente":
+        "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "Serviços Sociais":
+        "https://images.unsplash.com/photo-1582213782179-e0d53f98f2ca?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      Tecnologia:
+        "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      Cultura:
+        "https://images.unsplash.com/photo-1543906965-f9520aa2ed8a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "Planejamento Urbano":
+        "https://images.unsplash.com/photo-1505521501006-4c7fabcd65f2?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "Direitos Humanos":
+        "https://images.unsplash.com/photo-1629753908080-e8551ac57b8d?q=80&w=1175&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "Segurança Alimentar":
+        "https://images.unsplash.com/photo-1461354464878-ad92f492a5a0?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    };
+
     const formattedStartDate = new Date(startDate).toISOString();
     const formattedEndDate = new Date(endDate).toISOString();
+
+    const defaultImage =
+      defaultImageForField[fieldOfAction as FieldOfActionKey];
+
     const body = {
       title,
       description,
@@ -107,7 +132,7 @@ export default async function postActivity(
       tags,
       status: "open",
       pontuation: (actionType === "volunteer" ? 200 : 100) as number,
-      photos: ["https://images.unsplash.com/photo-1576675466684-456bcdeccfbf?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"],
+      photos: [defaultImage],
       ong_id: user.ong.id,
     };
 
@@ -124,7 +149,7 @@ export default async function postActivity(
       method: "POST",
       headers: {
         Authorization: `Bearer ${token.value}`,
-        "Content-Type": "application/json", 
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(body),
     });

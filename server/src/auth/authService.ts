8860c5f015878c12abcff2b7b6cf3e7199af0717
Felipe Leite <felipesd.leite@gmail.com>
2025-09-01 13:02:34 -0300

    fix: solving problems

--- server/src/auth/authService.ts
+++ server/src/auth/authService.ts
@@ -4,6 +4,7 @@ import { LoginDto } from './authDto';
 import axios from 'axios';
 import { OngService } from '../ong/ongService';
 import { UserService } from '../user/userService';
+import { hashPassword } from '../utils/bcrypt.utils';
 
 const prisma = new PrismaClient();
 
@@ -19,56 +20,55 @@ export class AuthService {
   async verifyOng(loginData: LoginDto): Promise<void> {
     const response = await axios.post('https://bora-impactar-dev.setd.rdmapps.com.br/api/login', {
       email: loginData.email,
-      password: loginData.password
+      password: loginData.password,
     });
-
-    //console.log(response.data);
 
     if (response.status !== 200) {
       throw new Error('ONG não encontrada');
-    } else {
-      const user = await prisma.user.findFirst({
-        where: { email: loginData.email },
-        include: { school: true, ong: true },
-      });
-      if(!user){
-        const apiData = response.data;
-        await prisma.$transaction(async (tx) => {
-          const ongCreated = await tx.ong.create({
-            data:{
-              id: 0, // Temporary id, will be ignored by the database auto-increment
-              name: apiData.ngo.name,
-              description: apiData.ngo.description,
-              start_year: apiData.ngo.start_year,
-              phone_number: apiData.ngo.contact_phone,
-              social_medias: [apiData.ngo.instagram_link, apiData.ngo.site],
-            }
-          });
-          //console.log(ongCreated)
-          const userCreated = await tx.user.create({
-            data: {
-              name: apiData.user.name,
-              email: apiData.user.email,
-              password: loginData.password,
-              ong_id: ongCreated.id,
-            }
-          });
-
-          return { ongCreated, userCreated };
-        }); 
-      } 
     }
+
+    const apiData = response.data;
+
+    await prisma.$transaction(async (tx) => {
+      const ongCreated = await tx.ong.create({
+        data: {
+          name: apiData.ngo.name,
+          description: apiData.ngo.description,
+          start_year: apiData.ngo.start_year,
+          phone_number: apiData.ngo.contact_phone,
+          social_medias: [apiData.ngo.instagram_link, apiData.ngo.site],
+        },
+      });
+
+      const hashedPassword = await hashPassword(loginData.password);
+
+      await tx.user.create({
+        data: {
+          name: apiData.user.name,
+          email: apiData.user.email,
+          password: hashedPassword,
+          ong_id: ongCreated.id,
+        },
+      });
+    });
   }
   
   async login(loginData: LoginDto) {
 
-    await this.verifyOng(loginData);
-
-    const user = await prisma.user.findFirst({
+    let user = await prisma.user.findFirst({
       where: { email: loginData.email },
       include: { school: true, ong: true },
     });
-    console.log(user);
+
+    if (!user) {
+      await this.verifyOng(loginData);
+
+      user = await prisma.user.findFirst({
+        where: { email: loginData.email },
+        include: { school: true, ong: true },
+      });
+    }
+
     if (!user) {
       throw new Error('Usuário não encontrado');
     }
@@ -77,7 +77,7 @@ export class AuthService {
       loginData.password,
       user.password,
     );
-    console.log(passwordMatch);
+
     if (!passwordMatch) {
       throw new Error('Senha inválida');
     }
